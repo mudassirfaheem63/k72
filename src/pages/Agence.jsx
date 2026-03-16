88be26a806dbe3377e7e2cd11d15819aee268f79
mudassirfaheem63 <mudassirfaheem63@gmail.com>
2026-03-17 02:11:50 +0500

    sec-change

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>
-      <div className='h-[20vw] rounded-3xl  overflow-hidden w-[15vw] top-96 left-[30vw] absolute'>
+   <div>
+     <div id='section1'>
+      <div className='h-[20vw] rounded-3xl  overflow-hidden w-[15vw] top-50 left-[30vw] absolute'>
         <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
       </div>
       <div className='relative font-[font2]'>
@@ -11,12 +12,16 @@ const Agence = () => {
       <h1 className='text-[19vw]  text-center uppercase leading-[18vw]'>Soixan7e <br />Douze</h1>
      </div>
      <div className='pl-[50%] mt-20'> 
-       <p className='text-6xl'>
+       <p className='text-3xl'>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
       </p>
      </div>
     </div>
     </div>
+    <div id="section2" className='h-screen'>
+      
+    </div>
+   </div>
   )
 }
 
