86f4fb70fda70cf5920cf12181f66fd4dbaf99eb
mudassirfaheem63 <mudassirfaheem63@gmail.com>
2026-03-19 16:15:35 +0500

    p

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Video = () => {
   return (
      <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+        {/* Change from "./video.mp4" to one of these: */}
+        <video 
+          className='h-full w-full object-cover' 
+          autoPlay 
+          loop 
+          muted 
+          src={`${import.meta.env.BASE_URL}video.mp4`}
+        ></video>
     </div>
   )
 }
